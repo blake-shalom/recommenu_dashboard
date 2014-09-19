d37104e2afd19c9343882965c8666a982b142c7b
Carlos Balderas <caisbalderas@gmail.com>
2014-09-19 12:57:24 -0500

    updated raty image paths

--- app/scripts/directives/raty.js
+++ app/scripts/directives/raty.js
@@ -11,9 +11,9 @@ angular.module('recommenuClientDashApp')
     return {
       restrict: 'AE',
       link: function (scope, element, attrs) {
-        $.fn.raty.defaults.starOn  = './bower_components/raty/lib/images/star-on.png';
-        $.fn.raty.defaults.starOff  = './bower_components/raty/lib/images/star-off.png';
-        $.fn.raty.defaults.starHalf  = './bower_components/raty/lib/images/star-half.png';
+        $.fn.raty.defaults.starOn  = '../../images/star-on.png';
+        $.fn.raty.defaults.starOff  = '../../images/star-off.png';
+        $.fn.raty.defaults.starHalf  = '../../images/star-half.png';
         $.fn.raty.defaults.halfShow  = true;
         $.fn.raty.defaults.half  = true;
         $.fn.raty.defaults.readOnly  = true;
